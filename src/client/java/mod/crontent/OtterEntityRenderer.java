6449cfa65119110e933d7c8297a7e95b4830dcec
lukas <179175900+crontent@users.noreply.github.com>
2024-09-07 19:57:45 +0200

    Otter can now pickup items and hold them in his mouth

--- src/client/java/mod/crontent/OtterEntityRenderer.java
+++ src/client/java/mod/crontent/OtterEntityRenderer.java
@@ -1,11 +1,42 @@
 package mod.crontent;
 
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.BlockAndItemGeoLayer;
+
+import java.util.Objects;
 
 public class OtterEntityRenderer extends GeoEntityRenderer<OtterEntity> {
 
     public OtterEntityRenderer(EntityRendererFactory.Context renderManager) {
         super(renderManager, new OtterEntityModel());
+        addRenderLayer(new BlockAndItemGeoLayer<>(this){
+            @Override
+            protected @Nullable ItemStack getStackForBone(GeoBone bone, OtterEntity otter) {
+                return Objects.equals(bone.getName(), "head_item") ? otter.getEquippedStack(EquipmentSlot.MAINHAND) : null;
+            }
+
+            @Override
+            protected void renderStackForBone(MatrixStack poseStack, GeoBone bone, ItemStack stack, OtterEntity animatable, VertexConsumerProvider bufferSource, float partialTick, int packedLight, int packedOverlay) {
+                //Quaternionf rotationX = new Quaternionf(new AxisAngle4f((float) Math.toRadians(90), 1, 0, 0));
+                //Quaternionf rotationY = new Quaternionf(new AxisAngle4f((float) Math.toRadians(90), 0, 0, 1));
+//
+                //rotationX.mul(rotationY);
+//
+                //poseStack.multiply(rotationX);
+                //poseStack.translate(0, 0, 2);
+                poseStack.scale(.3f, .3f, .3f);
+
+                super.renderStackForBone(poseStack, bone, stack, animatable, bufferSource, partialTick, packedLight, packedOverlay);
+            }
+        });
+
+
     }
 }
--- src/main/java/mod/crontent/OtterEntity.java
+++ src/main/java/mod/crontent/OtterEntity.java
@@ -1,29 +1,47 @@
 package mod.crontent;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.FleeEntityGoal;
-import net.minecraft.entity.ai.goal.LookAtEntityGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.ai.goal.WanderAroundGoal;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.ai.control.MoveControl;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.AmphibiousSwimNavigation;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class OtterEntity extends PathAwareEntity implements GeoEntity {
+import java.util.EnumSet;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class OtterEntity extends AnimalEntity implements GeoEntity {
     protected static final RawAnimation WALK_ANIM = RawAnimation.begin().thenLoop("walk_cycle");
 
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
+    static final Predicate<ItemEntity> PICKABLE_ITEMS_FILTER = item -> !item.cannotPickup() && item.isAlive();
+
     public OtterEntity(EntityType<? extends OtterEntity> entityType, World world) {
         super(entityType, world);
+        this.moveControl = new OtterEntity.OtterMoveControl(this);
+        this.setCanPickUpLoot(true);
     }
 
     public static DefaultAttributeContainer.Builder createOtterAttributes(){
@@ -34,14 +52,24 @@ public class OtterEntity extends PathAwareEntity implements GeoEntity {
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, .4f);
     }
 
+    @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
         this.goalSelector.add(1, new FleeEntityGoal<>(this, PlayerEntity.class, 10.0F, 1.6, 1.4, EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR::test));
         this.goalSelector.add(5, new WanderAroundGoal(this, 0.8D));
-        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 10.0F));
+        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 5.0F));
+        this.goalSelector.add(9, new OtterEntity.PickupItemGoal());
     }
 
+    @Override
+    protected void initEquipment(Random random, LocalDifficulty localDifficulty) {
+        this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(Items.DIAMOND));
+    }
 
+    @Override
+    protected EntityNavigation createNavigation(World world) {
+        return new OtterEntity.OtterSwimNavigation(this, world);
+    }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
@@ -60,6 +88,99 @@ public class OtterEntity extends PathAwareEntity implements GeoEntity {
         return this.cache;
     }
 
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return false;
+    }
 
+    @Nullable
+    @Override
+    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
+    }
+
+    boolean wantsToPickupItem() {
+        //TODO: check for states such as already moving towards an item, or searching
+        return true;
+    }
+
+    static class OtterMoveControl extends MoveControl{
+        private final OtterEntity otter;
+
+        OtterMoveControl(OtterEntity otter) {
+            super(otter);
+            this.otter = otter;
+        }
+
+        private void updateVelocity(){
+
+        }
+
+        @Override
+        public void tick() {
+            super.tick();
+        }
+    }
+
+    static class OtterSwimNavigation extends AmphibiousSwimNavigation {
+
+        public OtterSwimNavigation(MobEntity owner, World world) {
+            super(owner, world);
+        }
+
+        @Override
+        public boolean isValidPosition(BlockPos pos) {
+            return super.isValidPosition(pos);
+        }
+    }
+
+    class PickupItemGoal extends Goal {
+        public PickupItemGoal() {
+            this.setControls(EnumSet.of(Goal.Control.MOVE));
+        }
+
+        @Override
+        public boolean canStart() {
+            OtterEntity otter = OtterEntity.this;
+            if (!otter.getEquippedStack(EquipmentSlot.MAINHAND).isEmpty()){
+                return false;
+            } else if (otter.getTarget() != null || otter.getAttacker() != null){
+                return false;
+            } else if (!otter.wantsToPickupItem()){
+                return false;
+            } else if (otter.getRandom().nextInt(toGoalTicks(10)) != 0 ){
+                return false;
+            } else{
+                List<ItemEntity> itemsClose = getCloseItemEntities(otter);
+                return !itemsClose.isEmpty() && otter.getEquippedStack(EquipmentSlot.MAINHAND).isEmpty();
+            }
+        }
+
+        @Override
+        public void tick() {
+            OtterEntity otter = OtterEntity.this;
+            List<ItemEntity> itemsClose = getCloseItemEntities(otter);
+            ItemStack equippedStack = otter.getEquippedStack(EquipmentSlot.MAINHAND);
+            if(equippedStack.isEmpty() && !itemsClose.isEmpty()){
+                otter.getNavigation().startMovingTo(itemsClose.getFirst(), 1.2f);
+            }
+        }
+
+        private static List<ItemEntity> getCloseItemEntities(OtterEntity otter) {
+            return otter.getWorld()
+                    .getEntitiesByClass(ItemEntity.class,
+                            otter.getBoundingBox().expand(8d, 8d, 8d),
+                            OtterEntity.PICKABLE_ITEMS_FILTER);
+        }
+
+        @Override
+        public void start() {
+            OtterEntity otter = OtterEntity.this;
+            List<ItemEntity> itemsClose = getCloseItemEntities(otter);
+            if(!itemsClose.isEmpty()){
+                otter.getNavigation().startMovingTo(itemsClose.getFirst(), 1.2f);
+            }
+        }
+    }
 
 }
